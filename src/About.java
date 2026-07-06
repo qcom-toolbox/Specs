2e1c5b803077a06504b403f09390130883d92f8c
qualcomm-toolbox <169756160+qcom-toolbox@users.noreply.github.com>
2026-07-06 21:55:54 +0200

    Update About.java

--- src/About.java
+++ src/About.java
@@ -7,7 +7,7 @@ public class About {
     public static void showAbout(JFrame parent) {
         ImageIcon icon = new ImageIcon(Objects.requireNonNull(GUI.class.getResource("/icon/Icon 128x128.png")));
         JOptionPane.showMessageDialog(parent,
-                "Java Specs\nVersion 2.0\n© 2026",
+                "Java Specs\nVersion 2.0.0\n© 2026",
                 "About",
                 JOptionPane.INFORMATION_MESSAGE,
                 icon);
